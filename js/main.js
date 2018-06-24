b074c05e1691410022392487506437145c48559d
eduarmreyes <eduar.mreyes@gmail.com>
2018-06-23 18:47:02 -0600

    Show form as designed Display none results

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 (function() {
 	// todo: create class Calculator to calculate the mortage result
+	var calculator = document.getElementById('calculator');
+
+	calculator.addEventListener('submit', function(e) {
+		e.preventDefault();
+	});
+
+	var validate_form = function() {
+		// validate form
+	};
 })();
